00c764f782c24b0be0e2bc4bdd8689508444d8a0
denisailisie <55743053+denisailisie@users.noreply.github.com>
2020-04-05 08:27:25 +0300

    Edit

--- Cultural Hub/WebApp/Controllers/HomeController.cs
+++ Cultural Hub/WebApp/Controllers/HomeController.cs
@@ -28,6 +28,13 @@ namespace WebApp.Controllers
             return View();
         }
 
+        public IActionResult Labs()
+        {
+            return View();
+        }
+
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
